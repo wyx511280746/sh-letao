c057693bd12f5f73b2c397fcb7ff95f1934f933e
WYX <511280746@qq.com>
2018-06-25 19:27:13 +0800

    登录表单验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -4,11 +4,11 @@ $(function () {
       // excluded: [':disabled', ':hidden', ':not(:visible)'],//排除无需验证的控件，比如被禁用的或者被隐藏的  
       // submitButtons: '#btn-test',//指定提交按钮，如果验证失败则变成disabled，但我没试成功，反而加了这句话非submit按钮也会提交到action指定页面  
       // message: '通用的验证失败消息',//好像从来没出现过  
-      // feedbackIcons: {//根据验证结果显示的各种图标  
-      //     valid: 'glyphicon glyphicon-ok',  
-      //     invalid: 'glyphicon glyphicon-remove',  
-      //     validating: 'glyphicon glyphicon-refresh'  
-      // },  
+      feedbackIcons: {//根据验证结果显示的各种图标  
+          valid: 'glyphicon glyphicon-ok',  
+          invalid: 'glyphicon glyphicon-remove',  
+          validating: 'glyphicon glyphicon-refresh'  
+      },  
       fields: {  
           username: {  
               validators: {  
@@ -18,8 +18,11 @@ $(function () {
                   stringLength: {//检测长度  
                       min: 2,  
                       max: 6,  
-                      message: '长度必须在6-30之间'  
+                      message: '长度必须在2-6之间'  
                   },  
+                  callback : {
+                    message : '用户名不存在'
+                  }
                   // regexp: {//正则验证  
                   //     regexp: /^[a-zA-Z0-9_\.]+$/,  
                   //     message: '所输入的字符不符要求'  
@@ -34,8 +37,11 @@ $(function () {
                   stringLength: {//检测长度  
                       min: 6,  
                       max: 12,  
-                      message: '长度必须在6-30之间'  
+                      message: '长度必须在6-12之间'  
                   },  
+                  callback : {
+                    message :'密码错误'
+                  }
                   // regexp: {//正则验证  
                   //     regexp: /^[a-zA-Z0-9_\.]+$/,  
                   //     message: '所输入的字符不符要求'  
@@ -45,10 +51,38 @@ $(function () {
           }  
       }  
   });  
-  $("#btn-test").click(function () {//非submit按钮点击后进行验证，如果是submit则无需此句直接验证  
-      $("#form-test").bootstrapValidator('validate');//提交验证  
-      if ($("#form-test").data('bootstrapValidator').isValid()) {//获取验证结果，如果成功，执行下面代码  
-          alert("yes");//验证成功后的操作，如ajax  
-      }  
-  });  
+  //使用ajax提交表单
+  $('#form').on('success.form.bv',function (e) {
+      //阻止表单提交
+    e.preventDefault();
+    console.log('阻止完成');
+    
+    //通过ajax提交
+    $.ajax({
+        type:'post',
+        url:'/employee/employeeLogin',
+        data:$('#form').serialize(),
+        dataType:"json",
+        success : function (info) {  
+            console.log(info);    
+          if(info.success ){
+            location.href = 'index.html';
+          }
+          if(info.error === 1001 ){
+            $('#form').data("bootstrapValidator").updateStatus('password','INVALID',"callback");
+          }
+          if(info.error === 1000 ){
+            $('#form').data("bootstrapValidator").updateStatus('username','INVALID',"callback");
+          }
+
+        }
+
+    })
+
+  })
+  //表单重置
+  $('[type="reset"]').click(function () {
+    $('#form').data('bootstrapValidator').resetForm(true);
+  })
+
 });  
